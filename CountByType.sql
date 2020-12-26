2de32d8f7f699e771253048392ea11f9c6384f8c
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-26 23:48:40 +0530

    [Pratibha Mastud] Add : Add Contacts in addressbook by Type

--- CountByType.sql
+++ CountByType.sql
@@ -1,2 +1,7 @@
-select count(addressBook_Type) from [dbo].[address_bookDB] where addressBook_Type = 'Family';
+insert into [dbo].[address_bookDB] values 
+('Akshay', 'rane','sawantwadi','nashik','maharashtra',2456,'987654324','akshayhran@3.gmail.com','RaneBook','Friends'),
+('mukesh', 'Patiane','wadi','kolapur','maharashtra',3456,'967654324','patihran@2.gmail.com','PatiBook','Family');
+;
+
 select * from [dbo].[address_bookDB];
+
