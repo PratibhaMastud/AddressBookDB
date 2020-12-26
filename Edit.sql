1a7bf67e9ed087fb9b4351ff7f6565e3ad90b308
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-26 17:14:32 +0530

    [Pratibha Mastud] Add : Update particular data using name in table

---
+++ Edit.sql
@@ -0,0 +1,4 @@
+update  [dbo].[address_bookDB] set address ='Nerul' where first_name = 'Prati';
+update  [dbo].[address_bookDB] set zip = 2123 where first_name = 'Prati';
+update  [dbo].[address_bookDB] set last_name = 'karande' where first_name = 'Prati';
+update  [dbo].[address_bookDB] set last_name = 'deshmukh' where first_name = 'Anu';
